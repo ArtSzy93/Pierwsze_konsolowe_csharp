64aaa345aa7b2e6b0fca9b1afdd01ac8ca8984bb
ArtySzy93 <szyplakowski.a@gmail.com>
2022-03-14 14:10:39 +0100

    Dodaj pliki projektów.

---
+++ Kolejny/Klasy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolejny
+{
+    internal class Klasy
+    {
+    }
+
+    public class Czlowiek
+    {
+        public Czlowiek() // Konstruktor
+        {
+
+        }
+        public string imie = "Nie podano";
+        public int wiek = 0;
+
+
+    }
+}
---
+++ Kolejny/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Kolejny // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek czlowiekAdam = new Czlowiek();
+            Czlowiek pusty = new Czlowiek();
+            czlowiekAdam.imie = "Adam";
+            czlowiekAdam.wiek = 23;
+            Console.WriteLine("Adam nazywa się teraz: " + czlowiekAdam.imie + " i ma lat: " + czlowiekAdam.wiek);
+            Console.WriteLine("Pusty ma na imię: " + pusty.imie + " i ma lat: " + pusty.wiek);
+
+
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
